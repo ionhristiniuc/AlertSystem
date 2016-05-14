77e0733431ab42949db86c86a42557227ad0cc8f
Ion Hristiniuc <ionhristiniuc@yahoo.com>
2016-05-15 01:29:30 +0300

    Implement authentication

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartup(typeof(WebApp.Startup))]
@@ -16,12 +23,12 @@ namespace WebApp
 
         public void ConfigureAuth(IAppBuilder app)
         {
-            //app.UseCookieAuthentication(new CookieAuthenticationOptions
-            //{
-            //    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-            //    LoginPath = new PathString("/Account/Login")
-            //});
-            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                CookieSecure = CookieSecureOption.SameAsRequest
+            });            
         }
     }
 }
