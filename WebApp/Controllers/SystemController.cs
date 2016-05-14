de624a965e228950a0acad1d693556fd14054aef
Ion Hristiniuc <ionhristiniuc@yahoo.com>
2016-05-14 14:10:20 +0300

    Model from database

---
+++ WebApp/Controllers/SystemController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class SystemController : ApiController
+    {
+        // Search notifications logic
+        // Update db with new notifications
+    }
+}
