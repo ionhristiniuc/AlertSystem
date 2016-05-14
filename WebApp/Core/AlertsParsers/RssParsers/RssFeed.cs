6e3435665cc5ee62c2296dbb1e6d5d4ef1acb7df
Adrian Trosin <adtrosin@endava.net>
2016-05-14 18:02:29 +0300

    modify rssfeed

--- WebApp/Core/AlertsParsers/RssParsers/RssFeed.cs
+++ WebApp/Core/AlertsParsers/RssParsers/RssFeed.cs
@@ -10,9 +10,17 @@ namespace WebApp.Core.AlertsParsers.RssParsers
     public class RssFeed : IAlertsParser
     {
         private string _url;
-        private List<Alert> alerts = new List<Alert>();
+        private List<Alert> alerts;
 
         public RssFeed(String url)
+        {
+            _url = url;
+            alerts = new List<Alert>();
+
+            ParseAlerts();
+        }
+
+        private void ParseAlerts()
         {
             var reader = new FeedReader();
             var items = reader.RetrieveFeed(_url);
@@ -22,13 +30,13 @@ namespace WebApp.Core.AlertsParsers.RssParsers
                 Alert alert = new Alert();
                 alert.Subject = i.Title;
                 alert.Text = i.Summary;
-                alert.Notify_time = i.Date.DateTime;
-                alerts.Add(alert);     
+                alert.Notify_time = i.Date.UtcDateTime;
+                alerts.Add(alert);
             }
         }
 
         public IEnumerable<Alert> GetAlerts()
-        {
+        {           
             return alerts;
         }
     }
